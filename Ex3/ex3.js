156cabe50be3ebbba83f4d65aaedcb013718e07c
JG-Souza <joaogsouzafma@gmail.com>
2024-09-08 10:12:59 -0300

    resolucao ex3

---
+++ Ex3/ex3.js
@@ -0,0 +1,5 @@
+function concatenaDados(nome, idade, cidade){
+    console.log(`Olá, meu nome é ${nome}, ` + `tenho ${idade} anos ` + `e nasci em ${cidade} `)
+}
+
+concatenaDados('João', 19, 'São Paulo');
