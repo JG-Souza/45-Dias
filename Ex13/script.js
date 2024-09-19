25ef3815a4325481ed1f2a88a69d074bceeab7ed
JG-Souza <joaogsouzafma@gmail.com>
2024-09-19 15:44:23 -0300

    resolução ex13

---
+++ Ex13/script.js
@@ -0,0 +1,10 @@
+let n = 8
+
+function converteAsteriscos() {
+    for (let i = 1; i <= n; i++) {
+        numeroAsteriscos = '*'.repeat(i)
+        console.log(numeroAsteriscos)
+    }
+}
+
+converteAsteriscos()
